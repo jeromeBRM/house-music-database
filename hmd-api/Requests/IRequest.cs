4e4488e79ca24a28c8d09300bd2a8090f23d15f4
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2023-03-07 19:14:03 +0100

    [US020] Create Request interface and abstract class
    - Added IRequest interface that implements Execute and SetParamaters methods.
    - Added abstract class Request that implements the IRequest interface.

---
+++ hmd-api/Requests/IRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hmd_api.Requests
+{
+    interface IRequest
+    {
+        public void Execute();
+        public void SetParameters(string[] parameters);
+    }
+}
