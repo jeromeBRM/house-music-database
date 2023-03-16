d755790ad9acca645c88b4fa24e05fe1ce6dd6b2
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2023-03-16 17:14:43 +0100

    [US029] Implement RestoreAll method in HmdAPI.cs
    - Implemented RestoreAll method for Track type.

---
+++ hmd-api/Requests/GetApiObjectsByTypeRequest.cs
@@ -0,0 +1,26 @@
+﻿using hmd_api.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace hmd_api.Requests
+{
+    internal class GetApiObjectsByTypeRequest : Request
+    {
+        public override List<SQLApiObject> Select(string[] parameters)
+        {
+            return HmdAPI.GetInstance().GetDbContext().SQLApiObjects.FromSqlRaw(
+                this.RequestBody(),
+                new SqliteParameter("@type", parameters[0]))
+                .ToList();
+        }
+
+        protected override string RequestBody()
+        {
+            return new String(@"select * from api_objects where type = @type;");
+        }
+    }
+}
