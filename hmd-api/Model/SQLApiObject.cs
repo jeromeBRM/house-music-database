9194a423de54368487364c42aa9d1cc0bfbe131c
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2023-03-17 19:04:31 +0100

    [IN005] Remove dictionary attribute of SQLApiObject.cs
    - Removed datas attribute.

--- hmd-api/Model/SQLApiObject.cs
+++ hmd-api/Model/SQLApiObject.cs
@@ -11,33 +11,13 @@ namespace hmd_api.Model
         public string type { get; set; }
         public string value { get; set; }
 
-        private Dictionary<string, object> datas;
-
         public SQLApiObject()
         {
-            this.datas = new Dictionary<string, object>();
         }
 
         public SQLApiObject(string value)
         {
-            this.datas = new Dictionary<string, object>();
             this.value = value;
-        }
-
-        public SQLApiObject AddProperty<T>(string key, T value)
-        {
-            this.datas.Add(key, value);
-            return this;
-        }
-
-        public Dictionary<string, object> Datas()
-        {
-            return this.datas;
-        }
-
-        public T GetProperty<T>(string key)
-        {
-            return (T) this.datas[key];
         }
     }
 }
