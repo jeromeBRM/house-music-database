e9f25f8d4b7fbe3e4160307a517b61eaef9e9c03
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2023-04-06 11:28:51 +0200

    [US031] Add table view for tracks
    - Created Tracklist component with column labels.
    - Added Tracklist component to main view.

---
+++ hmd-app/src/app/components/tracklist/tracklist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hmd-tracklist',
+  templateUrl: './tracklist.component.html',
+  styleUrls: ['./tracklist.component.css']
+})
+export class TracklistComponent {
+
+}
